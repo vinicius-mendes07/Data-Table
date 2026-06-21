762af1cec028e5695d6be014542bc18db709778c
Vinicius Mendes <vinicius.henrique.92@hotmail.com>
2026-06-21 08:38:16 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { InvoicesTable } from './components/InvoicesTable';
+
+export function App() {
+  return (
+    <div className="p-10">
+      <InvoicesTable />
+    </div>
+  );
+}
---
+++ src/components/InvoicesTable/index.tsx
@@ -0,0 +1,34 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { invoices } from './data';
+
+export function InvoicesTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Invoice</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead className="text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {invoices.map((invoice) => (
+          <TableRow key={invoice.invoice}>
+            <TableCell className="font-medium">{invoice.invoice}</TableCell>
+            <TableCell>{invoice.paymentStatus}</TableCell>
+            <TableCell>{invoice.paymentMethod}</TableCell>
+            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
